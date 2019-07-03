a7645837e9356c065fcde16aceff6e749b493163
Erik Hanchett <erik.hanchett@gmail.com>
2019-07-02 19:50:05 -0700

    Updated example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Random Dog Images</h1>
+    <h2>Dog Count {{counter}}</h2>
+    <button @click="getDog">Get Dogs</button>
+    <div class="dog-wrapper">
+      <div class="picture-dog">
+        <img :src="image" alt />
+      </div>
+      <button v-on:click="pressMe">Increment Dog Count</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  components: {},
+  created() {
+    this.getDog();
+  },
+  methods: {
+    pressMe() {
+      this.counter++;
+      console.log(this.counter);
+    },
+    getDog() {
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(res2 => {
+          this.image = res2.message;
+
+          this.counter++;
+        });
+    }
+  },
+  data() {
+    return {
+      counter: 0,
+      image: ""
+    };
   }
-}
+};
 </script>
+<style lang="css">
+button {
+  color: blue;
+  background-color: white;
+  border-radius: 10px;
+  font-size: 2rem;
+  border-style: solid;
+  border-color: bisque;
+  box-shadow: inset 0px 1px 0px rgba(75, 166, 255, 0.3);
+  outline: none;
+}
+
+button:hover {
+  text-shadow: 0px 0px 6px rgba(255, 255, 255, 1);
+  transition: all 0.4s ease 0s;
+  -webkit-box-shadow: 0px 5px 40px -10px rgba(0, 0, 0, 0.57);
+  -moz-box-shadow: 0px 5px 40px -10px rgba(0, 0, 0, 0.57);
+}
+img {
+  max-width: 30%;
+  padding: 30px;
+}
+* {
+  font-family: "Comic Sans MS", "Comic Sans", cursive;
+}
+h1 {
+  color: crimson;
+}
+</style>
