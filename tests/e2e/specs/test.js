76383091e3ffbbfbf516149eb360970467f765dc
Erik Hanchett <icystorm@gmail.com>
2019-07-07 09:08:41 -0700

    Added new tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,13 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
-  })
-})
+describe("My First Test", () => {
+  it("Visits the app root url", () => {
+    cy.visit("http://localhost:8080");
+    cy.contains("h1", "Random Dog Images");
+  });
+
+  it("pressing counter button increments button", () => {
+    cy.get("#myCounter").click();
+    cy.contains("h2", "Dog Count 2");
+  });
+});
