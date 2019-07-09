7e3f9ec8d7cf3c58effcbda92b3595e968311c59
Erik Hanchett <erik.hanchett@gmail.com>
2019-07-08 20:46:34 -0700

    removed text

--- tests/unit/Home.spec.js
+++ tests/unit/Home.spec.js
@@ -1,15 +1,19 @@
-import { expect } from "chai";
-import { shallowMount } from "@vue/test-utils";
-import Home from "@/views/Home.vue";
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
+import Home from '@/views/Home.vue';
 
-describe("Home.vue", () => {
-  it("h1 has correct h1 tag rendered", () => {
+describe('Home.vue', () => {
+  it('h1 has correct h1 tag rendered', () => {
     const wrapper = shallowMount(Home);
-    expect(wrapper.find("h1").text()).equal("Random Dog Images");
+    expect(wrapper.find('h1').text()).equal(
+      'Random Dog Images By Erik'
+    );
   });
 
-  it("button has correct text rendered", () => {
+  it('button has correct text rendered', () => {
     const wrapper = shallowMount(Home);
-    expect(wrapper.find("#myCounter").text()).equal("Increment Dog Count");
+    expect(wrapper.find('#myCounter').text()).equal(
+      'Increment Dog Count'
+    );
   });
 });
